4c7eeccaf745fa219bc33f36927679f39fb40b09
tdbaophuc <tdbaophuc@gmail.com>
2026-06-09 00:00:03 +0700

    feat: jdbc to jpa(páinitiation)

--- src/main/java/com/javaweb/repository/entity/DistrictEntity.java
+++ src/main/java/com/javaweb/repository/entity/DistrictEntity.java
@@ -1,10 +1,36 @@
 package com.javaweb.repository.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "district")
 public class DistrictEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "code")
     private String code;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "status")
     private Integer status;
+
+    @OneToMany(mappedBy = "district", fetch = FetchType.LAZY)
+    private List<BuildingEntity> building = new ArrayList<>();
+
+    public List<BuildingEntity> getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(List<BuildingEntity> building) {
+        this.building = building;
+    }
 
     public Integer getId() {
         return id;
--- src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
+++ src/main/java/com/javaweb/repository/impl/DistrictRepositoryImpl.java
@@ -3,8 +3,11 @@ package com.javaweb.repository.impl;
 import com.javaweb.repository.DistrictRepository;
 import com.javaweb.repository.entity.DistrictEntity;
 import com.javaweb.utils.ConnectionUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.SpringVersion;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -14,11 +17,15 @@ import java.util.List;
 
 @Repository
 public class DistrictRepositoryImpl implements DistrictRepository {
+
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public DistrictEntity findNameById(Integer id) {
         DistrictEntity districtEntity = new DistrictEntity();
         String SQL = "select d.name from district d where d.id="+id+" ";
-        try(Connection connection = ConnectionUtil.getConnection();
+        try(Connection connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery(SQL.toString())) {
 
